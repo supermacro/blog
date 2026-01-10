6befcb8d921c55282e80879a769a7560bc38b7b8
Giorgio Delgado <gio@coplane.com>
2026-01-09 20:37:12 -0500

    Link to actual site

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://gdelgado.ca',
 	integrations: [mdx(), react(), tailwind(), sitemap()],
 });
